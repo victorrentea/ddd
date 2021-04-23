1a8ea0fa313aef5787c280d5a56d7704240e6420
Victor Rentea <victorrentea@gmail.com>
2021-04-23 13:18:25 +0300

    Getting from static into into Spring context

--- ddd-application/src/main/java/victor/training/ddd/GlobalConfigParams.java
+++ ddd-application/src/main/java/victor/training/ddd/GlobalConfigParams.java
@@ -1,6 +1,9 @@
 package victor.training.ddd;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.ApplicationContext;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -16,12 +19,44 @@ public class GlobalConfigParams {
       GlobalConfigParams.flagX = flagX;
    }
 }
+@Service
+class GlobalConfigParamsInstance {
+   @Value("${flagX}")
+   private boolean flagX;
+
+   public boolean getFlagX() {
+      return flagX;
+   }
+}
+@Component
+class SpringGateway {
+   private static ApplicationContext spring;
+
+   @Autowired
+   public  void setSpring(ApplicationContext spring) {
+      SpringGateway.spring = spring;
+   }
+
+   public static ApplicationContext getSpring() {
+      return spring;
+   }
+
+   public static <T> T getBean(Class<T> clazz) {
+      return spring.getBean(clazz);
+   }
+   public static GlobalConfigParamsInstance getConfig() {
+      return spring.getBean(GlobalConfigParamsInstance.class);
+   }
+}
 // @Service
 class X {
    // @Autowired
    public static void oldMethod() {
       if (GlobalConfigParams.getFlagX()) {
          System.out.println("Hello");
+      }
+      if (SpringGateway.getConfig().getFlagX()) {
+         System.out.println("HelloInstance");
       }
          System.out.println("AFTER");
    }
