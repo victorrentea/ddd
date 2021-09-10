9e595ab32dc17019c319ebcd9d93230bf2f36362
Victor Rentea <victorrentea@gmail.com>
2021-09-10 13:45:21 +0300

    id

--- ddd-domain/src/main/java/victor/training/ddd/user/model/User.java
+++ ddd-domain/src/main/java/victor/training/ddd/user/model/User.java
@@ -1,15 +1,17 @@
 package victor.training.ddd.user.model;
 
+import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
-
-import java.util.UUID;
 
 @Document
 public class User {
 
    @Id
-   private String id = UUID.randomUUID().toString();
+   private ObjectId id;
+
+//   @Id
+//   private String id = UUID.randomUUID().toString();
 
    private String username;
 
