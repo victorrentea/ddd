500aa0b17b35eef90527fc2b5f7e0d9507930afc
Victor Rentea <victorrentea@gmail.com>
2021-11-23 15:44:06 +0200

    add p6spy

--- ddd-application/src/main/java/victor/training/ddd/Application.java
+++ ddd-application/src/main/java/victor/training/ddd/Application.java
@@ -3,27 +3,22 @@ package victor.training.ddd;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.stream.annotation.EnableBinding;
-import org.springframework.cloud.stream.annotation.Input;
-import org.springframework.cloud.stream.annotation.Output;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.messaging.MessageChannel;
-import org.springframework.messaging.SubscribableChannel;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.web.client.RestTemplate;
 
 
-interface AllChannels {
-   @Output("ordersConfirmedOut")
-   MessageChannel ordersConfirmedOut();
-
-   @Input("ordersConfirmedIn")
-   SubscribableChannel ordersConfirmedIn();
-}
-
-
-@EnableBinding(AllChannels.class)
+//interface AllChannels {
+//   @Output("ordersConfirmedOut")
+//   MessageChannel ordersConfirmedOut();
+//
+//   @Input("ordersConfirmedIn")
+//   SubscribableChannel ordersConfirmedIn();
+//}
+//
+//
+//@EnableBinding(AllChannels.class)
 @EnableAsync
 @SpringBootApplication
 @RequiredArgsConstructor
