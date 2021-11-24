20ffc308b71901dcf2d967265a51fa329457fb20
Victor Rentea <victorrentea@gmail.com>
2021-11-24 13:50:30 +0200

    add warmup - Developer

--- ddd-application/src/main/java/victor/training/ddd/agile/BacklogItem.java
+++ ddd-application/src/main/java/victor/training/ddd/agile/BacklogItem.java
@@ -78,22 +78,21 @@ class BacklogItem {
    private String title;
    private String description;
 
-   @ManyToOne
-   private Sprint sprint; // ⚠ not NULL when assigned to a sprint
-   private Integer fpEstimation; // ⚠ not NULL when assigned to a sprint
-
    public enum Status {
       CREATED,
       STARTED,
       DONE
    }
-
    @Enumerated(STRING)
    private Status status = Status.CREATED;
+
+   @ManyToOne
+   private Sprint sprint; // ⚠ not NULL when assigned to a sprint
+   private Integer fpEstimation; // ⚠ not NULL when assigned to a sprint
+   private Integer hoursConsumed; // ⚠ not NULL when assigned to a sprint
+
    @Version
    private Long version;
-
-   private int hoursConsumed;
 
    public void addHours(int hours) {
       hoursConsumed += hours;
---
+++ ddd-application/src/test/java/victor/training/ddd/user/DeveloperTest.java
@@ -0,0 +1,15 @@
+package victor.training.ddd.user;
+
+import org.junit.jupiter.api.Test;
+import victor.training.ddd.order.model.Customer;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DeveloperTest {
+   @Test
+   void test() {
+      Customer customer = new Customer();
+
+   }
+
+}
