d253b0d0ec1b40be27abf212d2d9939dcf53109b
Victor Rentea <victorrentea@gmail.com>
2022-03-02 01:37:29 +0200

    END

--- ddd-application/src/main/java/victor/training/ddd/agile/application/dto/ProductDto.java
+++ ddd-application/src/main/java/victor/training/ddd/agile/application/dto/ProductDto.java
@@ -4,12 +4,14 @@ import lombok.Data;
 import victor.training.ddd.agile.domain.model.Product;
 
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @Data
 
 public class ProductDto {
    public Long id;
    // @UniqueCode // custom constraint whose invisible magic Validator calls into DB
+   @Size(min = 10)
    public String code;
    @NotNull
    public String name;
