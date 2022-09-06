e18c95a4266fd602f244b955c80b8796c9ab382f
victorrentea <victorrentea@gmail.com>
2022-09-06 19:28:09 +0300

    Backlog item -> kt

--- ddd-application/src/main/java/victor/training/ddd/agile/entity/BacklogItem.kt
+++ ddd-application/src/main/java/victor/training/ddd/agile/entity/BacklogItem.kt
@@ -1,135 +1,46 @@
-package victor.training.ddd.agile.entity;
+package victor.training.ddd.agile.entity
 
-import javax.persistence.*;
-import javax.validation.constraints.NotNull;
-
-import static javax.persistence.EnumType.STRING;
+import javax.persistence.*
+import javax.validation.constraints.NotNull
 
 // Child entity of the "Sprint" Aggregate.
 @Entity
-public class BacklogItem {
-    @Id
-    @GeneratedValue
-    private Long id;
+class BacklogItem(
+    @field:ManyToOne val product: Product,
+    val title: @NotNull String?,
+    val description: String,
+    @Version val version: Long?,
+    @GeneratedValue @Id val id: Long?
+) {
+    enum class Status {
+        CREATED, STARTED, DONE
+    }
+
+    @Enumerated(EnumType.STRING)
+    var status = Status.CREATED
+
     @ManyToOne
-    private Product product;
+    // ⚠ not NULL when assigned to a sprint
+    var sprint: Sprint? = null
+    // ⚠ not NULL when assigned to a sprint
+    var fpEstimation: Int? = null
 
-    @NotNull
-    private String title;
-    private String description;
-    @Enumerated(STRING)
-    private Status status = Status.CREATED;
-    @ManyToOne
-    private Sprint sprint; // ⚠ not NULL when assigned to a sprint
-    private Integer fpEstimation; // ⚠ not NULL when assigned to a sprint
-    private int hoursConsumed;
-    @Version
-    private Long version;
+    private var hoursConsumed = 0
 
-    public BacklogItem() {
+
+    fun complete() {
+        check(status == Status.STARTED) { "Cannot complete an Item before starting it" }
+        status = Status.DONE
     }
 
-    public void complete() {
-        if (status != Status.STARTED) {
-          throw new IllegalStateException("Cannot complete an Item before starting it");
-       }
-        this.status = Status.DONE;
+    fun start() {
+        check(sprint!!.status == Sprint.Status.STARTED)
+        check(status == Status.CREATED) { "Item already started" }
+        status = Status.STARTED
     }
 
-     void start() {
-        if (sprint.getStatus() != Sprint.Status.STARTED) {
-            throw new IllegalStateException();
-        }
-        if (status != Status.CREATED) {
-          throw new IllegalStateException("Item already started");
-       }
-        this.status = Status.STARTED;
+    fun getHoursConsumed() = hoursConsumed
+    fun addHours(hours: Int) {
+        hoursConsumed += hours
     }
-
-    public Long getId() {
-        return id;
-    }
-
-    public BacklogItem setId(Long id) {
-        this.id = id;
-        return this;
-    }
-
-    public Product getProduct() {
-        return product;
-    }
-
-    public BacklogItem setProduct(Product product) {
-        this.product = product;
-        return this;
-    }
-
-    public @NotNull String getTitle() {
-        return title;
-    }
-
-    public BacklogItem setTitle(@NotNull String title) {
-        this.title = title;
-        return this;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public BacklogItem setDescription(String description) {
-        this.description = description;
-        return this;
-    }
-
-    public Status getStatus() {
-        return status;
-    }
-
-    public Sprint getSprint() {
-        return sprint;
-    }
-
-    public BacklogItem setSprint(Sprint sprint) {
-        this.sprint = sprint;
-        return this;
-    }
-
-    public Integer getFpEstimation() {
-        return fpEstimation;
-    }
-
-    public BacklogItem setFpEstimation(Integer fpEstimation) {
-        this.fpEstimation = fpEstimation;
-        return this;
-    }
-
-    public int getHoursConsumed() {
-        return hoursConsumed;
-    }
-
-    public BacklogItem setHoursConsumed(int hoursConsumed) {
-        this.hoursConsumed = hoursConsumed;
-        return this;
-    }
-
-    public Long getVersion() {
-        return version;
-    }
-
-    public BacklogItem setVersion(Long version) {
-        this.version = version;
-        return this;
-    }
-
-    public void addHours(int hours) {
-        hoursConsumed += hours;
-    }
-
-    public enum Status {
-        CREATED,
-        STARTED,
-        DONE
-    }
-
 }
--- ddd-application/src/main/java/victor/training/ddd/agile/service/BacklogItemService.kt
+++ ddd-application/src/main/java/victor/training/ddd/agile/service/BacklogItemService.kt
@@ -1,54 +1,47 @@
-package victor.training.ddd.agile.service;
+package victor.training.ddd.agile.service
 
-import lombok.RequiredArgsConstructor;
-import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.*;
-import victor.training.ddd.agile.entity.BacklogItem;
-import victor.training.ddd.agile.entity.Product;
-import victor.training.ddd.agile.dto.BacklogItemDto;
-import victor.training.ddd.agile.repo.BacklogItemRepo;
-import victor.training.ddd.agile.repo.ProductRepo;
+import org.springframework.transaction.annotation.Transactional
+import org.springframework.web.bind.annotation.*
+import victor.training.ddd.agile.dto.BacklogItemDto
+import victor.training.ddd.agile.entity.BacklogItem
+import victor.training.ddd.agile.repo.BacklogItemRepo
+import victor.training.ddd.agile.repo.ProductRepo
 
 @RestController
-@RequiredArgsConstructor
-public class BacklogItemService {
-   private final BacklogItemRepo backlogItemRepo;
-   private final ProductRepo productRepo;
+class BacklogItemService(private val backlogItemRepo: BacklogItemRepo,
+                         private val productRepo: ProductRepo) {
+    @PostMapping("backlog")
+    @Transactional
+    fun createBacklogItem(@RequestBody dto: BacklogItemDto): Long? {
+        val product = productRepo.findOneById(dto.productId)
+        val backlogItem = BacklogItem(product, dto.description, dto.title, null, null)
+        return backlogItemRepo.save(backlogItem).id
+    }
 
-   @PostMapping("backlog")
-   @Transactional
-   public Long createBacklogItem(@RequestBody BacklogItemDto dto) {
-      Product product = productRepo.findOneById(dto.getProductId());
-      BacklogItem backlogItem = new BacklogItem()
-          .setProduct(product)
-          .setDescription(dto.getDescription())
-          .setTitle(dto.getTitle());
-      return backlogItemRepo.save(backlogItem).getId();
-   }
+    @GetMapping("backlog/{id}")
+    fun getBacklogItem(@PathVariable id: Long): BacklogItemDto {
+        val backlogItem = backlogItemRepo.findOneById(id)
+        val dto = BacklogItemDto(backlogItem.product.id!!, backlogItem.title!!, backlogItem.description)
+        dto.id = backlogItem.id
+        dto.version = backlogItem.version
+        return dto
+    }
 
-   @GetMapping("backlog/{id}")
-   public BacklogItemDto getBacklogItem(@PathVariable long id) {
-      BacklogItem backlogItem = backlogItemRepo.findOneById(id);
-      BacklogItemDto dto = new BacklogItemDto(backlogItem.getProduct().getId(), backlogItem.getTitle(), backlogItem.getDescription());
-      dto.setId(backlogItem.getId());
-      dto.setVersion(backlogItem.getVersion());
-      return dto;
-   }
+    @PutMapping("backlog")
+    fun updateBacklogItem(@RequestBody dto: BacklogItemDto) {
+        // TODO if Backlog Item is COMPLETED, reject the update
+        val backlogItem = BacklogItem(
+            productRepo.findOneById(dto.productId),
+            dto.description,
+            dto.title,
+            dto.version,
+            dto.id
+        )
+        backlogItemRepo.save(backlogItem)
+    }
 
-   @PutMapping("backlog")
-   public void updateBacklogItem(@RequestBody BacklogItemDto dto) {
-      // TODO if Backlog Item is COMPLETED, reject the update
-      BacklogItem backlogItem = new BacklogItem()
-          .setId(dto.getId())
-          .setProduct(productRepo.findOneById(dto.getProductId()))
-          .setDescription(dto.getDescription())
-          .setTitle(dto.getTitle())
-          .setVersion(dto.getVersion());
-      backlogItemRepo.save(backlogItem);
-   }
-
-   @DeleteMapping("backlog/{id}")
-   public void deleteBacklogItem(@PathVariable long id) {
-      backlogItemRepo.deleteById(id);
-   }
+    @DeleteMapping("backlog/{id}")
+    fun deleteBacklogItem(@PathVariable id: Long) {
+        backlogItemRepo.deleteById(id)
+    }
 }
