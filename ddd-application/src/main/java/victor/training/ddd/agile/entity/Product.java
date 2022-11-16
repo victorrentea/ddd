d214341844f7d4c8952bdd21f03a99dee1f5bfc2
Victor Rentea <victorrentea@gmail.com>
2022-11-16 09:11:18 +0200

    TODO extract VO declutter the domain model

--- ddd-application/src/main/java/victor/training/ddd/agile/entity/Product.java
+++ ddd-application/src/main/java/victor/training/ddd/agile/entity/Product.java
@@ -1,16 +1,11 @@
 package victor.training.ddd.agile.entity;
 
-import lombok.Data;
-
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.OneToMany;
+import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
 
 
-@Data
+// consider encapsulating changes
 @Entity
 public class Product {
    @Id
@@ -20,6 +15,10 @@ public class Product {
    private int currentVersion = 0;
    private String code;
    private String name;
+
+   // TODO extract in an ProductOwner value object @Embeddable (no setters)
+//   @Embedded
+//   private ProductOwner owner;
 
    private String ownerEmail;
    private String ownerName;
@@ -41,6 +40,109 @@ public class Product {
    public int incrementAndGetVersion() {
       currentVersion++;
       return currentVersion;
+   }
+
+   public Long getId() {
+      return this.id;
+   }
+
+   public int getCurrentIteration() {
+      return this.currentIteration;
+   }
+
+   public int getCurrentVersion() {
+      return this.currentVersion;
+   }
+
+   public String getCode() {
+      return this.code;
+   }
+
+   public String getName() {
+      return this.name;
+   }
+
+   public String getOwnerEmail() {
+      return this.ownerEmail;
+   }
+
+   public String getOwnerName() {
+      return this.ownerName;
+   }
+
+   public String getOwnerPhone() {
+      return this.ownerPhone;
+   }
+
+   public String getTeamMailingList() {
+      return this.teamMailingList;
+   }
+
+   public List<Sprint> getSprints() {
+      return this.sprints;
+   }
+
+   public List<Release> getReleases() {
+      return this.releases;
+   }
+
+   public Product setId(Long id) {
+      this.id = id;
+      return this;
+   }
+
+   public Product setCurrentIteration(int currentIteration) {
+      this.currentIteration = currentIteration;
+      return this;
+   }
+
+   public Product setCurrentVersion(int currentVersion) {
+      this.currentVersion = currentVersion;
+      return this;
+   }
+
+   public Product setCode(String code) {
+      this.code = code;
+      return this;
+   }
+
+   public Product setName(String name) {
+      this.name = name;
+      return this;
+   }
+
+   public Product setOwnerEmail(String ownerEmail) {
+      this.ownerEmail = ownerEmail;
+      return this;
+   }
+
+   public Product setOwnerName(String ownerName) {
+      this.ownerName = ownerName;
+      return this;
+   }
+
+   public Product setOwnerPhone(String ownerPhone) {
+      this.ownerPhone = ownerPhone;
+      return this;
+   }
+
+   public Product setTeamMailingList(String teamMailingList) {
+      this.teamMailingList = teamMailingList;
+      return this;
+   }
+
+   public Product setSprints(List<Sprint> sprints) {
+      this.sprints = sprints;
+      return this;
+   }
+
+   public Product setReleases(List<Release> releases) {
+      this.releases = releases;
+      return this;
+   }
+
+   public String toString() {
+      return "Product(id=" + this.getId() + ", currentIteration=" + this.getCurrentIteration() + ", currentVersion=" + this.getCurrentVersion() + ", code=" + this.getCode() + ", name=" + this.getName() + ", ownerEmail=" + this.getOwnerEmail() + ", ownerName=" + this.getOwnerName() + ", ownerPhone=" + this.getOwnerPhone() + ", teamMailingList=" + this.getTeamMailingList() + ")";
    }
 }
 
